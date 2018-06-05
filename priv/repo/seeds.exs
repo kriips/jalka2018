59649faf2471fc7e1c71b64800d251353cb70356
Mikk <mikk@heathmont.net>
2018-06-05 23:36:40 +0300

    poke seeds back

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,6 +10,35 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
+if Code.ensure_compiled?(Pokedex.Pokemons.Species) &&
+     Pokedex.Pokemons.Species |> Pokedex.Repo.aggregate(:count, :id) == 0 do
+  Enum.each(Jason.decode!(File.read!('priv/repo/pokemons.json')), fn attrs ->
+    %Pokedex.Pokemons.Species{}
+    |> Pokedex.Pokemons.Species.changeset(attrs)
+    |> Pokedex.Repo.insert!()
+  end)
+end
+
+if Code.ensure_compiled?(Pokedex.Pokemons.Nest) &&
+     Pokedex.Pokemons.Nest |> Pokedex.Repo.aggregate(:count, :id) == 0 do
+  Enum.each(Jason.decode!(File.read!('priv/repo/tokens.json')), fn nest_data ->
+    nest =
+      %Pokedex.Pokemons.Nest{}
+      |> Pokedex.Pokemons.Nest.changeset(%{id: nest_data["id"], token: nest_data["token"]})
+      |> Pokedex.Repo.insert!()
+
+    for species_id <- nest_data["species_ids"] do
+      %Pokedex.Pokemons.Nesting{
+        nest: nest,
+        species_id: species_id,
+        probability: :rand.uniform(20) + 1
+      }
+      |> Pokedex.Repo.insert!()
+    end
+  end)
+end
+
+
 if Code.ensure_compiled?(Pokedex.Accounts.AllowedUser) &&
      Pokedex.Accounts.AllowedUser |> Pokedex.Repo.aggregate(:count, :id) == 0 do
   Enum.each(Jason.decode!(File.read!('priv/repo/allowed_users.json')), fn attrs ->
