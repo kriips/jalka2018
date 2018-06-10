8e4936d7d87f174d148c3b662d5a6eff4145c0e8
Mikk <mikk@heathmont.net>
2018-06-10 18:37:57 +0300

    heroku setup

---
+++ config/prod.secret.exs
@@ -0,0 +1,18 @@
+use Mix.Config
+
+
+config :pokedex, PokedexWeb.Endpoint,
+       load_from_system_env: true,
+         # Don't forget to replace pure-peak-67829 with the name of your Phoenix application that was created.
+       url: [scheme: "https", host: "fast-spire-67955.herokuapp.com", port: 80],
+       cache_static_manifest: "priv/static/cache_manifest.json",
+       secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
+config :pokedex, Pokedex.Repo,
+       adapter: Ecto.Adapters.Postgres,
+       url: System.get_env("DATABASE_URL"),
+       pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+       ssl: true
+
+
+config :logger, level: :info
